b0c9a2421f1aa63847062c25db00ac21b70efbb2
Guilherme Braga <gbp1999@gmail.com>
2019-11-23 23:01:53 -0300

    Add files via upload

--- source/funcoes_auxiliares.c
+++ source/funcoes_auxiliares.c
@@ -3,6 +3,74 @@
 #include <string.h>
 #include <stdint.h>
 
+#define LIMITE 10
+
+void wait(uint16_t input){
+
+    volatile uint16_t dt;
+    dt = input;
+
+    TB1CTL = TBSSEL__SMCLK | MC__UP | TBCLR;
+    TB1CCR0 = dt;
+
+    while(!(TB1CCTL0 & CCIFG));
+    TB1CCTL0 &= ~CCIFG;
+
+
+}
+
+inline void trava_milissegundos(long long int milissegundos){
+
+    // conta at� 8 segundos
+    // recebo um long int
+
+    // 8192 = ACLK / 4
+    // milisegundos = ((batidas de clock * 1000) / 8192)
+    // batidas de clock = ((milissegundos * 8192) / 1000)
+    // o nosso CCR0!
+
+    volatile unsigned long int batidas_de_clock;
+    batidas_de_clock = (milissegundos * 8.192);
+
+    TB1CTL = (TBSSEL__ACLK | ID__4 | MC__CONTINOUS | TBCLR);
+    TB1CCR0 = batidas_de_clock - 1;
+
+    while(!(TB0CCTL0 & CCIFG));
+    TB1CCTL0 &= ~CCIFG;
+
+}
+
+
+
+uint8_t verifica_dist(){
+
+  volatile uint8_t tempo;
+  tempo = 0;
+
+  //__disable_interrupt();
+
+
+  P2OUT |= BIT0;
+  wait(20);
+  P2OUT &= ~BIT0;
+
+  while(!(P2IN & BIT2));      //espero meu pino conectado ao echo receber um sinal
+
+  TB1CTL = (TBSSEL__SMCLK | MC__CONTINOUS | TBCLR);
+
+  while(P2IN & BIT2);     // espero parar de receber um sinal
+
+  tempo = TB1R;    // batidas de clock
+
+
+  //while(1);   // no lugar disso aqui eu ligaria os leds de acordo e tal
+
+  //__enable_interrupt();
+
+
+  return tempo; 
+}
+
 float valor_normalizado_vetor_potenciometro(uint16_t vector[8]) {
 
     float media = 0;
@@ -24,33 +92,18 @@ float valor_normalizado_vetor_LDR(uint16_t vector[8], uint8_t *luz_baixa) {
         media += vector[k];
     }
 
-    if(media > 20000){
+    if(media > 7400){
       *luz_baixa = 1;
     }
     else{
       *luz_baixa = 0;
     }
 
-    return ((media)/8000);       // 0.92 vem de uma regra de 3, o LCD estava lendo valores acima de 3,3V, então assim diminuimos o valor
+    return ((media));       // 0.92 vem de uma regra de 3, o LCD estava lendo valores acima de 3,3V, então assim diminuimos o valor
     // dividimos por 8 pois temos 8 amostras
     // dividimos por mil pois os valores saiam na casa do milhares
 
 }
-
-void wait(uint16_t input){
-
-    volatile uint16_t dt;
-    dt = input;
-
-    TB1CTL = TBSSEL__SMCLK | MC__UP | TBCLR;
-    TB1CCR0 = dt;
-
-    while(!(TB1CCTL0 & CCIFG));
-    TB1CCTL0 &= ~CCIFG;
-
-
-}
-
 
 // função de leitura ADC single channel, single convertion
 uint16_t adcRead(uint8_t pin)
@@ -73,27 +126,30 @@ uint16_t adcRead(uint8_t pin)
     return ADCMEM0;                             // retornamos o espaço de memória com o resultado
 }
 
-void convert_deci_hex(int n_decimal, char* resposta){
+uint8_t verifica_falso_positivo(){
 
-    static int quociente, restante;
-    unsigned int i, j = 0;
-    char dec_hex[4];            // tamanho do que queremos converter
+    volatile uint8_t teste;
 
-    quociente = n_decimal;
+    P6OUT |= BIT6;
 
-    while (quociente != 0)          // fazemos para o numero inteiro
-    {
-        restante = quociente % 16;      // dividimos em potencias de 16
-        if (restante < 10)
-            dec_hex[j++] = 48 + restante;     // colocamos de acordo com a tablea ascii
-        else
-            dec_hex[j++] = 55 + restante;
-        quociente = quociente / 16;
+    trava_milissegundos(1500);
+
+    P6OUT &= ~BIT6;
+
+    trava_milissegundos(500);
+
+    teste = verifica_dist();
+
+    if(teste > LIMITE){
+
+        return 0;
+
     }
 
-    for(i = 0; i < j; i++) {
-        resposta[i] = dec_hex[j - 1 - i];                           // colocamos a respostas no vetor
+    else{
+
+        return 1;
+
     }
-    resposta[j] = '\0';
 
 }
